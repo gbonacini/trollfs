b83de4ac48f0f58c959b0f51c16915af63dc2e9c
BG <bg@Gabrieles-MacBook-Pro.local>
2017-05-30 20:49:54 +0200

    Added examples.

---
+++ trollrepos/trollface.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+   (void)puts("PROBLEMS? :-)");
+   return 0;
+}
---
+++ trollrepos/trollface.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+   std::cout << "PROBLEMS? :-)" << std::endl;
+   return 0;
+}
